bf608a7b41b9c3d9925a0d11710140aa53a0f5a6
Chris Bell <chris@copperorange.com>
2025-12-09 00:46:00 -0500

    Revert to original working code from quest repo

--- src/000.js
+++ src/000.js
@@ -1,45 +1,50 @@
 const express = require('express')
-const { execFile } = require('child_process')
-const helmet = require('helmet')
-const rateLimit = require('express-rate-limit')
 const app = express()
-const port = process.env.PORT || 3000
+const port = 3000
 
-app.use(helmet())
-app.use(express.json())
-app.use(rateLimit({ windowMs: 60 * 1000, max: 60 }))
-
-function runScript(res, scriptPath, args = []) {
-  execFile(scriptPath, args, { windowsHide: true }, (err, stdout, stderr) => {
-    if (err) {
-      console.error(`error running ${scriptPath}:`, err)
-      return res.status(500).send(stderr || 'Internal error')
-    }
-    return res.send(stdout)
-  })
-}
-
-app.get('/', (req, res) => runScript(res, 'bin/001'))
-app.get('/aws', (req, res) => runScript(res, 'bin/002'))
-app.get('/docker', (req, res) => runScript(res, 'bin/003'))
-
-app.get('/loadbalanced', (req, res) => {
-  // Only pass a safe subset of headers for demo purposes
-  const safeHeaders = {
-    host: req.headers.host,
-    'user-agent': req.headers['user-agent']
+app.get('/', function (req, res) {
+const { exec } = require('child_process');
+exec('bin/001', (err, stdout, stderr) => {
+  if (err) {
+    return res.send(`${stderr}`);
   }
-  runScript(res, 'bin/004', [JSON.stringify(safeHeaders)])
-})
+  return res.send(`${stdout}`);
+});
+});
 
-app.get('/tls', (req, res) => {
-  const safeHeaders = { host: req.headers.host }
-  runScript(res, 'bin/005', [JSON.stringify(safeHeaders)])
-})
+app.get('/aws', function (req,res) {
+const { exec } = require('child_process');
+exec('bin/002', (err, stdout, stderr) => {
+  return res.send(`${stdout}`);
+});
+});
 
-app.get('/secret_word', (req, res) => {
-  // Do not accept secret via headers; this demo reads from env only
-  runScript(res, 'bin/006', [process.env.SECRET_WORD || ''])
-})
+app.get('/docker', function (req,res) {
+const { exec } = require('child_process');
+exec('bin/003', (err, stdout, stderr) => {
+  return res.send(`${stdout}`);
+});
+});
+
+app.get('/loadbalanced', function (req,res) {
+const { exec } = require('child_process');
+exec('bin/004 ' + JSON.stringify(req.headers), (err, stdout, stderr) => {
+  return res.send(`${stdout}`);
+});
+});
+
+app.get('/tls', function (req,res) {
+const { exec } = require('child_process');
+exec('bin/005 ' + JSON.stringify(req.headers), (err, stdout, stderr) => {
+  return res.send(`${stdout}`);
+});
+});
+
+app.get('/secret_word', function (req,res) {
+const { exec } = require('child_process');
+exec('bin/006 ' + JSON.stringify(req.headers), (err, stdout, stderr) => {
+  return res.send(`${stdout}`);
+});
+});
 
 app.listen(port, () => console.log(`Rearc quest listening on port ${port}!`))
